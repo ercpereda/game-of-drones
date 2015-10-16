7d2a4971191c0266296313c50ac1392b84a11dff
ercpereda <ercp42@gmail.com>
2015-10-16 14:34:04 -0400

    created data dbcontext

---
+++ Source/GoD.Data/GoDContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoD.Domain;
+
+namespace GoD.Data
+{
+    public class GoDContext : DbContext
+    {
+        public GoDContext()
+            : base("DefaultConnection")
+        {}
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Game> Games { get; set; }
+    }
+}
