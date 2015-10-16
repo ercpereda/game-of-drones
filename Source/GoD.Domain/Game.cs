859dfd5f0a012e5364a93921dac7fa9f51358fc0
ercpereda <ercp42@gmail.com>
2015-10-16 14:30:03 -0400

    created domain models

---
+++ Source/GoD.Domain/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoD.Domain
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+    }
+}
---
+++ Source/GoD.Domain/GameScore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoD.Domain
+{
+    public class GameScore
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public Player Player { get; set; }
+        public Game Game { get; set; }
+    }
+}
