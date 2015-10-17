fccbc213a29863681bed0a5f3ba7f63a48e67a2c
ercpereda <ercp42@gmail.com>
2015-10-17 01:48:22 -0400

    created select players controller

---
+++ Source/GoD.Web.AngularApp/src/local/controllers/select-players-controller.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    angular
+        .module('gameOfDrones.local')
+        .controller('SelectPlayersController', selectsPlayersController);
+
+    function selectsPlayersController() {
+        var vm = this;
+
+        vm.player1 = 'player1';
+        vm.player2 = 'player2';
+        vm.startGame = startGame;
+
+        function startGame() {
+            
+        }
+    }
+})()
