63d549e2494175aefb3b8ce3a88d2814662f1be1
ercpereda <ercp42@gmail.com>
2015-10-17 16:29:02 -0400

    created rules controller

---
+++ Source/GoD.Web.Api/Controllers/RulesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GoD.Domain;
+
+namespace GoD.Web.Api.Controllers
+{
+    [RoutePrefix("api/Rules")]
+    public class RulesController : ApiController
+    {
+        private readonly IRepository _repository;
+
+        public RulesController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_repository.Rules);
+        }
+    }
+}
