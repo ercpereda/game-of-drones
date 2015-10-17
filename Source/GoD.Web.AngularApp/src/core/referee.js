6e1629199a59ac5f28084a482334ac07ee4c46bd
ercpereda <ercp42@gmail.com>
2015-10-17 07:42:41 -0400

    created referee service

---
+++ Source/GoD.Web.AngularApp/src/core/referee.js
@@ -0,0 +1,43 @@
+﻿(function() {
+    angular
+        .module('gameOfDrones.core')
+        .provider('referee', refereeProvider);
+
+    function refereeProvider() {
+        var provider = {
+            baseUrl: 'http://localhost:3000/',
+            decideUrl: 'api/Referee/Decide',
+            setBaseUrl: function (url) { provider.baseUrl = url; },
+            setDecideUrl: function (url) { provider.decideUrl = url; },
+            $get: refereeService
+        }
+
+        return provider;
+
+        refereeService.$inject = ['$http', '$q', '$log'];
+
+        function refereeService($http, $q, $log) {
+            var service = {
+                decide: decide
+            }
+
+            return service;
+
+            function decide(player1Move, player2Move) {
+                var deferred = $q.defer();
+
+                $http({
+                    method: 'GET',
+                    url: provider.baseUrl + provider.decideUrl,
+                    data: { player1Move: player1Move, player2Move: player2Move }
+                }).subccess(function(data, status, headers, cfg) {
+                    deferred.resolve(data);
+                }).error(function(data, status, headers, cfg) {
+                    deferred.reject(status);
+                });
+
+                return deferred.promise;
+            }
+        }
+    }
+});
