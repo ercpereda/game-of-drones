6d5f55b62cfc3e0acaa7ec10870a0630621ee45a
ercpereda <ercp42@gmail.com>
2015-10-17 10:53:40 -0400

    created referee controller

---
+++ Source/GoD.Web.Api/Controllers/RefereeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GoD.Web.Api.Controllers
+{
+    [RoutePrefix("api/Referee")]
+    public class RefereeController : ApiController
+    {
+        public IHttpActionResult GetDecide(string player1Move, string player2Move)
+        {
+            return Ok(1);
+        }
+    }
+}
