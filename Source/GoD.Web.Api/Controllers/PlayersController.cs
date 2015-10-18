822b3ee389fa9e886a21158c0a37dc91c4b985fa
ercpereda <ercp42@gmail.com>
2015-10-18 11:48:52 -0400

    created players controller

---
+++ Source/GoD.Web.Api/Controllers/PlayersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GoD.Domain;
+
+namespace GoD.Web.Api.Controllers
+{
+    [RoutePrefix("api/Players")]
+    public class PlayersController : ApiController
+    {
+        private readonly IRepository _repository;
+
+        public PlayersController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [Route("Ranking")]
+        public IHttpActionResult GetRaking()
+        {
+            return Ok();
+        }
+    }
+}
