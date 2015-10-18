806e2c88f310d7bae9d052848a5f43c4ddea644c
ercpereda <ercp42@gmail.com>
2015-10-18 16:24:51 -0400

    created main controller for the application

---
+++ Source/GoD.Web.AngularApp/src/game-of-drones-main-controller.js
@@ -0,0 +1,35 @@
+﻿(function() {
+    angular
+        .module('gameOfDrones')
+        .controller('AppMainController', appMainController);
+
+    appMainController.$inject = ['$mdSidenav', '$location'];
+
+    function appMainController($mdSidenav, $location) {
+        var vm = this;
+
+        vm.toggleSidenav = function (sideNavId) { $mdSidenav(sideNavId).toggle(); }
+        vm.go = function(url) { $location.path(url); }
+
+        vm.menu = [
+            {
+                name: "Game",
+                items: [
+                  { name: "Star Game", url: "local/selectplayers", icon: "gamepad" }
+                ]
+            },
+            {
+                name: "Players",
+                items: [
+                    { name: "Ranking", url: "players/ranking", icon: "group" }
+                ]
+            },
+            {
+                name: "Settings",
+                items: [
+                    { name: "Rules Set", url: "rules/manage", icon: "settings" }
+                ]
+            }
+        ];
+    }
+})()
