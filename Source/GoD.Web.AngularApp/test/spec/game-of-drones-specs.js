d5e5e149d5769f9149d024e512466790116e2b59
ercpereda <ercp42@gmail.com>
2015-10-19 06:31:51 -0400

    configured testing

---
+++ Source/GoD.Web.AngularApp/test/spec/game-of-drones-specs.js
@@ -0,0 +1,7 @@
+﻿describe('MainCtrl', function () {
+    beforeEach(module('gameOfDrones'));
+
+    it('should assign the correct rapper to scope', function () {
+        expect(true).toEqual(true);
+    });
+});
